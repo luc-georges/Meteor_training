0bfbed5f110ef5521995c2839d1d419d4dd827c9
luc <luc.georges.ld@gmail.com>
2021-04-17 12:58:26 +0200

    add possibility to remove task

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,8 +5,11 @@ import { Task } from './Task';
 import { TaskForm } from './TaskForm';
 
 export const App = () => {
+  //get collection and sort it , starting from the most recent one
   const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
-
+  //delete the task using it ID
+  const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+  //set the isChecked property of the task by his id
   const toggleChecked = ({ _id, isChecked }) => {
     TasksCollection.update(_id, {
       $set: {
@@ -20,7 +23,11 @@ export const App = () => {
       <h1>Welcome to Meteor!</h1>
       <TaskForm/>
       <ul>
-        { tasks.map(task => <Task key={ task._id } task={ task } onCheckboxClick={toggleChecked} />) }
+        { tasks.map(task => <Task
+          key={ task._id } 
+          task={ task } 
+          onCheckboxClick={toggleChecked}
+          onDeleteClick={deleteTask} />) }
       </ul>
     </div>
   );
